2f8af533dcf4eeb46d393d176eb4785166e57952
JayasreeSuryadevara <sjayas@hotmail.com>
2020-03-16 15:24:00 -0700

    User Auth Frontend

--- rise.js
+++ rise.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
