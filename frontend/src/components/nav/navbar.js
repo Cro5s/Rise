6e39210408fac1b7fb885677896a43df513cb6ae
JayasreeSuryadevara <sjayas@hotmail.com>
2020-03-18 13:43:07 -0700

    Create Navbar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -40,6 +40,69 @@ class NavBar extends React.Component {
           <div className="navbar-icon">
             <i className="fas fa-bars"></i>
           </div>
+          <div className="category-menu-navbar">
+            <nav className="navbar" role="navigation">
+              <ul className="category-menu">
+                <li className="category-menu-item-1" >
+                  <Link to="/woman" className="category-link-1">WOMAN</Link>
+                  <li class="menu-item--is-divider" role="separator"/>
+                  <li class="menu-item--is-divider" role="separator" />
+                  <ul className="menu-item-1-subitems">
+                    <li className="category-menu-item-1-1">
+                      <Link to="/woman/jackets" className="category-link-1-1">JACKETS</Link>
+                    </li>
+                    <li className="category-menu-item-1-2">
+                      <Link to="/woman/shirts" className="category-link-1-2">SHIRTS</Link>
+                    </li>
+                    <li className="category-menu-item-1-3">
+                      <Link to="/woman/pants" className="category-link-1-3">PANTS</Link>
+                    </li>
+                    <li className="category-menu-item-1-4">
+                      <Link to="/woman/shoes" className="category-link-1-4">SHOES</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="category-menu-item-2" >
+                  <Link to="/men" className="category-link-2">MEN</Link>
+                  <li class="menu-item--is-divider" role="separator" />
+                  <li class="menu-item--is-divider" role="separator" />
+                  <ul className="menu-item-2-subitems">
+                    <li className="category-menu-item-2-1">
+                      <Link to="/men/jackets" className="category-link-2-1">JACKETS</Link>
+                    </li>
+                    <li className="category-menu-item-2-2">
+                      <Link to="/men/shirts" className="category-link-2-2">SHIRTS</Link>
+                    </li>
+                    <li className="category-menu-item-2-3">
+                      <Link to="/men/pants" className="category-link-2-3">PANTS</Link>
+                    </li>
+                    <li className="category-menu-item-2-4">
+                      <Link to="/men/shoes" className="category-link-2-4">SHOES</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="category-menu-item-3" >
+                  <Link to="/kids" className="category-link-3">WOMAN</Link>
+                  <li class="menu-item--is-divider" role="separator" />
+                  <li class="menu-item--is-divider" role="separator" />
+                  <ul className="menu-item-3-subitems">
+                    <li className="category-menu-item-3-1">
+                      <Link to="/kids/jackets" className="category-link-3-1">JACKETS</Link>
+                    </li>
+                    <li className="category-menu-item-3-2">
+                      <Link to="/kids/shirts" className="category-link-3-2">SHIRTS</Link>
+                    </li>
+                    <li className="category-menu-item-3-3">
+                      <Link to="/kids/pants" className="category-link-3-3">PANTS</Link>
+                    </li>
+                    <li className="category-menu-item-3-4">
+                      <Link to="/kids/shoes" className="category-link-3-4">SHOES</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
           {/* { this.getLinks() } */}
         </div>
         <div className="main-page-nav">
@@ -49,9 +112,11 @@ class NavBar extends React.Component {
             </Link>
           </div>
           <div className="status-container">
+            <div className="login-status-container">
             { 
-              currentUser ? <h3 className="user-name">{currentUserName}</h3> : <Link className="login-link" to="/login">LOG IN</Link>
+              currentUser.id ? <h3 className="user-name">{currentUserName}</h3> : <Link className="login-link" to="/login">LOG IN</Link>
             }
+            </div>
             <div 
               className="shopping-cart-icon"
             >
