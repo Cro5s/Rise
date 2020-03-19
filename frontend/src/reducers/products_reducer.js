028198fff23a21d5a895b308f0aebf1ffd82f670
Kadeem Jackson <kdjackson35@gmail.com>
2020-03-19 12:07:38 -0400

    Add productReducer and imported it into the root reducer

---
+++ frontend/src/reducers/products_reducer.js
@@ -0,0 +1,22 @@
+import { 
+  RECEIVE_PRODUCTS, 
+  RECEIVE_PRODUCT,  
+  RECEIVE_PRODUCT_TYPES
+} from "../actions/products_actions";
+
+export default function productsReducer(state = {}, action) {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_PRODUCT:
+      newState[action.product.id] = action.product;
+      return newState;
+    case RECEIVE_PRODUCTS:
+      return action.products;
+    case RECEIVE_PRODUCT_TYPES:
+      return action.products;
+    default:
+      return state;
+  };
+}
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
+import ProductsRecducer from "./session_reducer";
 
 const RootReducer = combineReducers({
   session: SessionReducer,
-  errors: ErrorsReducer
+  products: ProductsRecducer,
+  errors: ErrorsReducer,
 });
 
 export default RootReducer;
