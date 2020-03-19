8600aa92106d853c2cd926753eb848fa9363e8b1
rapkat10 <aminrapkat@gmail.com>
2020-03-19 09:47:28 -0600

    add products api util

---
+++ frontend/src/util/product_api_util.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+//Category index page
+export const getProducts = (category) => {
+    return axios.get(`/api/products/${category}`);
+};
+
+//Products index page
+export const getProductTypes = (category, productType) => {
+    return axios.get(`/api/products/${category}/${productType}`);
+};
+
+//Product show page
+export const getProduct = (Id) => {
+    return axios.get(`/api/products/${Id}`);
+};
+
