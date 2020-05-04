e4133a590f9d00563b0127b3cc592a5593ad166c
Kadeem Jackson <kdjackson35@gmail.com>
2020-05-04 19:47:05 -0400

    Add log out button to nav bar

--- frontend/src/actions/cart_item_actions.js
+++ frontend/src/actions/cart_item_actions.js
@@ -1,55 +1,48 @@
-import * as CartApiUtil from '../util/cart_api_util';
+import * as CartApiUtil from "../util/cart_api_util";
 
 export const RECEIVE_CART_ITEMS = "RECEIVE_CART_ITEMS";
 export const RECEIVE_CART_ITEM = "RECEIVE_CART_ITEM";
 export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM";
 
-export const receiveCartItems = cartItems => {
+export const receiveCartItems = (cartItems) => {
   return {
     type: RECEIVE_CART_ITEMS,
-    cartItems
-  }
-};
-
-export const receiveCartItem = cartItems => {
-  return {
-    type: RECEIVE_CART_ITEM,
-    cartItems
-  }
-};
-
-export const removeCartItem = cartItemId => {
-  return {
-    type: REMOVE_CART_ITEM,
-    cartItemId
+    cartItems,
   };
 };
 
-export const fetchCartItems = userId => dispatch => (
-    CartApiUtil.fetchCartItems(userId)
-    .then(res => dispatch(receiveCartItems(res.data)))
-    .catch(err => console.log(err))
-);
-
-export const updateCartItem = (id, data) => dispatch => {
-  return (
-    CartApiUtil.updateCartItem(id, data)
-    .then(res => dispatch(receiveCartItem(res.data)))
-    .catch(err => console.log(err))
-  );
+export const receiveCartItem = (cartItems) => {
+  return {
+    type: RECEIVE_CART_ITEM,
+    cartItems,
+  };
 };
 
-export const createCartItem = (id, data) => dispatch => {
-  return (
-    CartApiUtil.createCartItem(id, data)
-    .then(res => dispatch(receiveCartItem(res.data)))
-    .catch(err => console.log(err))
-  );
-
+export const removeCartItem = (cartItemId) => {
+  return {
+    type: REMOVE_CART_ITEM,
+    cartItemId,
+  };
 };
 
-export const deleteCartItem = id => dispatch => (
-    CartApiUtil.deleteCartItem(id)
+export const fetchCartItems = (userId) => (dispatch) =>
+  CartApiUtil.fetchCartItems(userId)
+    .then((res) => dispatch(receiveCartItems(res.data)))
+    .catch((err) => console.log(err));
+
+export const updateCartItem = (id, data) => (dispatch) => {
+  return CartApiUtil.updateCartItem(id, data)
+    .then((res) => dispatch(receiveCartItem(res.data)))
+    .catch((err) => console.log(err));
+};
+
+export const createCartItem = (id, data) => (dispatch) => {
+  return CartApiUtil.createCartItem(id, data)
+    .then((res) => dispatch(receiveCartItem(res.data)))
+    .catch((err) => console.log(err));
+};
+
+export const deleteCartItem = (id) => (dispatch) =>
+  CartApiUtil.deleteCartItem(id)
     .then(() => dispatch(removeCartItem(id)))
-    .catch(err => console.log(err))
-);
+    .catch((err) => console.log(err));
--- frontend/src/components/session/login_form_container.js
+++ frontend/src/components/session/login_form_container.js
@@ -1,21 +1,17 @@
-import { connect } from 'react-redux';
-import { login } from '../../actions/session_actions';
-import LoginForm from './login_form';
+import { connect } from "react-redux";
+import { login } from "../../actions/session_actions";
+import LoginForm from "./login_form";
 
 const mapStateToProps = (state) => {
-  console.log("state in login", state);
   return {
-    errors: state.errors.session
+    errors: state.errors.session,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    login: user => dispatch(login(user))
-  }
-}
+    login: (user) => dispatch(login(user)),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginForm);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
